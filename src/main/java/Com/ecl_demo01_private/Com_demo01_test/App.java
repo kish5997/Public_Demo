7ba7538d444fd290c7353a1ebba8e9a2c4dff0ab
kish5997 <kish5997@gmail.com>
2021-01-13 14:54:22 +0530

    "commit"

---
+++ src/main/java/Com/ecl_demo01_private/Com_demo01_test/App.java
@@ -0,0 +1,13 @@
+package Com.ecl_demo01_private.Com_demo01_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/add/Main.java
@@ -0,0 +1,11 @@
+package add;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
